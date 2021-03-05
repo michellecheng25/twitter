2becf96b196095bdf3eb036f38fbf90128da113b
Michelle Cheng <michellecheng.2017@gmail.com>
2021-03-04 19:55:45 -0500

    User can retweet a tweet.

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -19,26 +19,42 @@ class TweetCellTableViewCell: UITableViewCell {
     var favorited:Bool = false
     var tweetId:Int = -1
     
+    var retweeted:Bool = false;
+
+    
     @IBAction func favoriteTweet(_ sender: Any) {
         let tobeFavorited = !favorited
         if(tobeFavorited){
             TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
                 self.setFavorited(true)
             }, failure: { (Error) in
-                print("Favorite did not succeed \(Error)")
+                print("Favorite did not succeed: \(Error)")
             })
         }else{
             TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
                 self.setFavorited(false)
             }, failure: { (Error) in
-                print("Unfavorited did not succeed \(Error)")
+                print("Unfavorited did not succeed: \(Error)")
             })
         }
     }
     @IBAction func retweet(_ sender: Any) {
-        
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweeted(true)
+        }, failure: { (Error) in
+            print("Error in retweeting: \(Error)")
+        })
     }
     
+    func setRetweeted(_ isRetweeted:Bool){
+        if(isRetweeted){
+            retweetButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        }else{
+            retweetButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+            retweetButton.isEnabled = true
+        }
+    }
     
     func setFavorited(_ isFavorited:Bool){
         favorited = isFavorited
